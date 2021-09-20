bae983ce1e5440f2418cdd93e7ca6f0f782a3bef
jupmorenor <jupmorenor@gmail.com>
2021-09-20 10:55:43 -0500

    feat: Se agrega control de resultados de consulta vacios. #5

--- helpers/refactor_helper.go
+++ helpers/refactor_helper.go
@@ -2,19 +2,28 @@ package helpers
 
 import (
 	"encoding/json"
-	_ "fmt"
+	"fmt"
 )
 
 func LimpiezaRespuestaRefactor(respuesta map[string]interface{}, v interface{}) {
 	//fmt.Println("CAMBIO RESPUESTA")
 	//fmt.Println("respuesta")
 	//fmt.Println(respuesta)
-	b, err := json.Marshal(respuesta["Data"])
-	//fmt.Println(b)
-	if err != nil {
-		panic(err)
+	/**
+	* Cuando La petición no retorna datos, es decir, llega vacia no se carga la estructura
+	**/
+	datatype := fmt.Sprintf("%v", respuesta["Data"])
+	//fmt.Println(datatype)
+	switch datatype {
+	case "map[]", "[map[]]": // response vacio
+		break
+	default:
+		b, err := json.Marshal(respuesta["Data"])
+		//fmt.Println(b)
+		if err != nil {
+			panic(err)
+		}
+		json.Unmarshal(b, v)
 	}
-	json.Unmarshal(b, v)
 	//fmt.Println(v)
 }
-
